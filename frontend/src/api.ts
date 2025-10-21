bc3cbc00fcc24596a956262b6ca9c1de5a98c4b5
DanyHB <danielhalabi5@gmail.com>
2025-10-21 17:17:22 +0300

    Add HomePage FrontEnd

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,18 +1,18 @@
 import axios from 'axios'
-import type { apiAuthType } from './types';
+import type { User } from './types';
 
 
 
 const API_URL = import.meta.env.VITE_API_URL;
 
 
-export async function signup({ email, password, name }: apiAuthType) {
+export async function signup({ email, password, name }: User) {
   const res = await axios.post(`${API_URL}/auth/signup`, { email, password, name })
   return res.data;
 }
 
 
-export async function login({ email, password }: apiAuthType) {
+export async function login({ email, password }: User) {
   const res = await axios.post(`${API_URL}/auth/login`, { email, password })
   return res.data
 }
