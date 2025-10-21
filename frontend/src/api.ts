269c91dc40c85c2f1670e53f1d7b956533cf874f
DanyHB <danielhalabi5@gmail.com>
2025-10-21 13:43:13 +0300

    add auth API functions for signup and login

---
+++ frontend/src/api.ts
@@ -0,0 +1,20 @@
+import axios from 'axios'
+import type { apiAuthType } from './types';
+
+
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+
+export async function signup({ email, password, name }: apiAuthType) {
+  const res = await axios.post(`${API_URL}/auth/signup`, { email, password, name })
+  return res.data;
+}
+
+
+export async function login({ email, password }: apiAuthType) {
+  const res = await axios.post(`${API_URL}/auth/login`, { email, password })
+  return res.data
+}
+
+
